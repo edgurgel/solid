d1356d451950d66fd0f49464ce8139d4c27997db
Eduardo Gurgel <eduardo@gurgel.me>
2017-02-26 19:20:47 +1300

    Add access support to arrays through Solid.Argument

--- lib/solid/argument.ex
+++ lib/solid/argument.ex
@@ -13,11 +13,23 @@ defmodule Solid.Argument do
   123
   iex> Solid.Argument.get({:value, "value"}, %Solid.Context{})
   "value"
+  iex> Solid.Argument.get({:field, "key", [{:access, 1},{:access, 1}]}, %Solid.Context{vars: %{"key" => [1, [1,2,3], 3]}})
+  2
+  iex> Solid.Argument.get({:field, "key", [{:access, 1}]}, %Solid.Context{vars: %{"key" => "a string"}})
+  nil
   """
-  @spec get({:field, String.t} | {:value, term}, Context.t) :: term
+  @spec get({:field, String.t} | {:field, String.t, [{:access, non_neg_integer}]} | {:value, term}, Context.t) :: term
+  def get({:value, val}, _hash), do: val
   def get({:field, key}, %Context{vars: vars}) do
     key = key |> String.split(".")
     get_in(vars, key)
   end
-  def get({:value, val}, _hash), do: val
+  def get({:field, key, accesses}, context) do
+    value = get({:field, key}, context)
+    Enum.reduce(accesses, value,
+                fn {:access, index}, acc when is_list(acc) ->
+                     Enum.at(acc, index)
+                   _, _ -> nil
+                end)
+  end
 end
