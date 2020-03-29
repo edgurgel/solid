8074d27b718289342f8e011d24b8d18fd0b02ead
Eduardo Gurgel <eduardo@gurgel.me>
2020-03-29 16:45:04 +1300

    Bump version 0.4.1

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Solid.Mixfile do
   def project do
     [
       app: :solid,
-      version: "0.4.0",
+      version: "0.4.1",
       elixir: "~> 1.9",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
