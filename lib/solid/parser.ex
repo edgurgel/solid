285538814832832af819185c9de5fe1baa9d22d5
Mayank Gureja <mayankgureja@users.noreply.github.com>
2021-03-08 01:37:28 +0530

    Add missing identifier "?" (#64)

--- lib/solid/parser.ex
+++ lib/solid/parser.ex
@@ -11,7 +11,7 @@ defmodule Solid.Parser.Base do
         end
       end
 
-      identifier = ascii_string([?a..?z, ?A..?Z, ?0..?9, ?_, ?-], min: 1)
+      identifier = ascii_string([?a..?z, ?A..?Z, ?0..?9, ?_, ?-, ??], min: 1)
 
       plus = string("+")
       minus = string("-")
