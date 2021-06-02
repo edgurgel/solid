cd61bd65a1334363b6440494372e7ca648d7c9d2
Joel Ambass <Jcambass@users.noreply.github.com>
2021-06-02 12:49:57 +0200

    Tests, Add New assert_render helper (#74)

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -20,4 +20,35 @@ defmodule Solid.Helpers do
   def liquid_render(input_liquid, input_json) do
     System.cmd("ruby", ["test/liquid.rb", input_liquid, input_json])
   end
+
+  defmacro assert_render(liquid_input, json_input) do
+    quote do
+      solid_output =
+        render(unquote(liquid_input), Jason.decode!(unquote(json_input)), [])
+        |> IO.iodata_to_binary()
+
+      {liquid_output, 0} = liquid_render(unquote(liquid_input), unquote(json_input))
+
+      if liquid_output == solid_output do
+        true
+      else
+        message = """
+        Render result was different!
+        Input:
+        #{unquote(liquid_input)}
+        """
+
+        expr =
+          quote do
+            liquid_output == solid_output
+          end
+
+        raise ExUnit.AssertionError,
+          expr: expr,
+          left: liquid_output,
+          right: solid_output,
+          message: message
+      end
+    end
+  end
 end
