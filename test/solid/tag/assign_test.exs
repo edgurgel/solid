656d25d57b1b44e7453232b09fde4bb652cd1696
Eduardo <eduardo@sendle.com>
2021-09-18 18:50:48 +1200

    Add basic tag test

---
+++ test/solid/tag/assign_test.exs
@@ -0,0 +1,18 @@
+defmodule Solid.Tag.AssignTest do
+  use ExUnit.Case, async: true
+  alias Solid.Tag.Assign
+  alias Solid.Context
+
+  defmodule Parser do
+    import NimbleParsec
+    defparsec(:parse, Assign.spec(__MODULE__) |> eos())
+  end
+
+  test "integration" do
+    {:ok, parsed, _, _, _, _} = "{% assign first = 3 %}" |> Parser.parse()
+
+    assert {nil, context} = Assign.render(parsed, %Context{}, [])
+
+    assert context.vars == %{"first" => 3}
+  end
+end
