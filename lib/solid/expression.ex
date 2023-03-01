e53a07aed424ca576e522e29017d373e0a17443e
Fluke Kitisak <kitisak.spnr@gmail.com>
2023-03-01 11:02:19 +0700

    Contains are only force to_stirng

--- lib/solid/expression.ex
+++ lib/solid/expression.ex
@@ -74,7 +74,7 @@ defmodule Solid.Expression do
   def eval({nil, :contains, _v2}), do: false
   def eval({_v1, :contains, nil}), do: false
   def eval({v1, :contains, v2}) when is_list(v1), do: v2 in v1
-  def eval({v1, :contains, v2}), do: String.contains?(v1, v2)
+  def eval({v1, :contains, v2}), do: String.contains?(to_string(v1), to_string(v2))
   def eval({v1, :<=, nil}) when is_number(v1), do: false
   def eval({v1, :<, nil}) when is_number(v1), do: false
   def eval({nil, :>=, v2}) when is_number(v2), do: false
