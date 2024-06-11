df6c9d687c92434491526be28518e7e6a88e23a6
Niklas Lindgren <niklas@musicglue.com>
2024-06-11 15:20:28 +0300

    test(matchers): add cases for built-in matchers

--- test/matcher_test.exs
+++ test/matcher_test.exs
@@ -1,38 +1,80 @@
 defmodule MatcherTest do
   use ExUnit.Case, async: true
+  import Solid.Helpers
 
-  defmodule UserProfile do
-    defstruct [:full_name]
+  describe "custom matchers" do
+    defmodule UserProfile do
+      defstruct [:full_name]
 
-    defimpl Solid.Matcher do
-      def match(user_profile, ["full_name"]), do: {:ok, user_profile.full_name}
+      defimpl Solid.Matcher do
+        def match(user_profile, ["full_name"]), do: {:ok, user_profile.full_name}
+      end
+    end
+
+    defmodule User do
+      defstruct [:email]
+
+      def load_profile(%User{} = _user) do
+        # implementation omitted
+        %UserProfile{full_name: "John Doe"}
+      end
+
+      defimpl Solid.Matcher do
+        def match(user, ["email"]), do: {:ok, user.email}
+
+        def match(user, ["profile" | keys]),
+          do: user |> User.load_profile() |> @protocol.match(keys)
+      end
+    end
+
+    test "should render protocolized struct correctly" do
+      template = ~s({{ user.email }}: {{ user.profile.full_name }})
+
+      context = %{"user" => %User{email: "test@example.com"}}
+
+      assert "test@example.com: John Doe" == render(template, context)
     end
   end
 
-  defmodule User do
-    defstruct [:email]
+  describe "built-in matchers" do
+    test "for maps" do
+      template = ~s({{ beep.boop }})
 
-    def load_profile(%User{} = _user) do
-      # implementation omitted
-      %UserProfile{full_name: "John Doe"}
+      context = %{"beep" => %{"boop" => "beep boop!"}}
+
+      assert "beep boop!" == render(template, context)
     end
 
-    defimpl Solid.Matcher do
-      def match(user, ["email"]), do: {:ok, user.email}
+    test "for strings" do
+      template =
+        ~s(How long is {{piece_of_string}}? {{piece_of_string.size}}.)
 
-      def match(user, ["profile" | keys]),
-        do: user |> User.load_profile() |> @protocol.match(keys)
+      context = %{"piece_of_string" => "a piece of string"}
+
+      assert "How long is a piece of string? 17." == render(template, context)
     end
-  end
 
-  test "should render protocolized struct correctly" do
-    template = ~s({{ user.email }}: {{ user.profile.full_name }})
+    test "for lists" do
+      template = ~s(This eagle is just {{items.size}} {{items[1]}}s in a trenchcoat.)
 
-    context = %{
-      "user" => %User{email: "test@example.com"}
-    }
+      context = %{
+        "items" => ~w(This parrot has ceased to be.)
+      }
 
-    assert "test@example.com: John Doe" ==
-             template |> Solid.parse!() |> Solid.render!(context) |> to_string()
+      assert "This eagle is just 6 parrots in a trenchcoat." == render(template, context)
+    end
+
+    test "for atoms" do
+      Enum.each(
+        [
+          {~s({{molecule.atom.particle}}),
+           %{"molecule" => %{"atom" => %{"particle" => :neutron}}}, "neutron"},
+           {~s({{beep.boop}}), %{"beep" => nil}, ""}
+        ],
+        fn {template, context, expected} ->
+          assert expected == render(template, context)
+        end
+      )
+    end
   end
 end
